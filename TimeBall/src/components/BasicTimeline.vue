2dffece11c6f014446fc27c1c42e4373c35428d1
AtomicJellyfish <Jacob.a.coulson@pm.me>
2024-04-16 00:02:28 -0400

    INIT
    ROLLEM

---
+++ TimeBall/src/components/BasicTimeline.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+    <Timeline :value="events">
+    
+    
+    </Timeline>        
+    </div>
+</template>
+<script lang="ts">
+
+import Timeline from 'primevue/timeline';
+import events from '../../../TimeyWimeys/events.json'
+console.log(events);
+export default{
+    Timeline
+}
+
+</script>
+<style lang="scss">
+    
+</style>
